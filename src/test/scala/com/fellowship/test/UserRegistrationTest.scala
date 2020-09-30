9a4bdb7989af53c09d613cb25fbcef064cf57ae2
cheluvesha <cheluvesh.b@gmail.com>
2020-09-30 19:09:46 +0530

    [CHELUVESHA]Refactored : Made object reference as Global

--- src/test/scala/com/fellowship/test/UserRegistrationTest.scala
+++ src/test/scala/com/fellowship/test/UserRegistrationTest.scala
@@ -3,21 +3,17 @@ import com.fellowship.UserRegistration
 import org.scalatest.FunSuite
 
 class UserRegistrationTest extends FunSuite{
-
+  val userRegistration = new UserRegistration()
   test ("givenFirstNameWhenValidReturnTrue") {
-    val userRegistration = new UserRegistration()
     assert(userRegistration.validateFirstName("Cheluvesha") === true)
   }
   test ("givenFirstNameWhenNotValidReturnFalse") {
-    val userRegistration = new UserRegistration()
     assert(userRegistration.validateFirstName("Ch") === false)
   }
   test ("givenLastNameWhenValidReturnTrue") {
-    val userRegistration = new UserRegistration()
     assert(userRegistration.validateLastName("Bharath") === true)
   }
   test ("givenLastNameWhenNotValidReturnFalse") {
-    val userRegistration = new UserRegistration()
     assert(userRegistration.validateLastName("Bh") === false)
   }
 }
