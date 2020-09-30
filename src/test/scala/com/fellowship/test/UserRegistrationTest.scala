d16806c9b156408fbd057d8b4a27256a7ede3674
cheluvesha <cheluvesh.b@gmail.com>
2020-09-30 19:17:20 +0530

    [CHELUVESHA]Refactored : Made object reference as Global

--- src/test/scala/com/fellowship/test/UserRegistrationTest.scala
+++ src/test/scala/com/fellowship/test/UserRegistrationTest.scala
@@ -3,29 +3,23 @@ import com.fellowship.UserRegistration
 import org.scalatest.FunSuite
 
 class UserRegistrationTest extends FunSuite{
-
+  val userRegistration = new UserRegistration()
   test ("givenFirstNameWhenValidReturnTrue") {
-    val userRegistration = new UserRegistration()
     assert(userRegistration.validateFirstName("Cheluvesha") === true)
   }
   test ("givenFirstNameWhenNotValidReturnFalse") {
-    val userRegistration = new UserRegistration()
-    assert(userRegistration.validateFirstName("Ch") === false)
+    assert(userRegistration.validateFirstName(" $asdA4") === false)
   }
   test ("givenLastNameWhenValidReturnTrue") {
-    val userRegistration = new UserRegistration()
     assert(userRegistration.validateLastName("Bharath") === true)
   }
   test ("givenLastNameWhenNotValidReturnFalse") {
-    val userRegistration = new UserRegistration()
-    assert(userRegistration.validateLastName("Bh") === false)
+    assert(userRegistration.validateLastName("B131  ") === false)
   }
   test ("givenEmailWhenValidReturnTrue") {
-    val userRegistration = new UserRegistration()
     assert(userRegistration.validateEmail("abc.xyz@bl.com.in") === true)
   }
   test ("givenEmailWhenNotValidReturnFalse") {
-    val userRegistration = new UserRegistration()
     assert(userRegistration.validateEmail("com.in") === false)
   }
 }
