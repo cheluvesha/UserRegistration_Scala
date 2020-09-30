8fc747acc624799247dc443b7b2a7585bb3ce7b6
cheluvesha <cheluvesh.b@gmail.com>
2020-09-30 12:16:09 +0530

    [CHELUVESHA]Added : Method comments.

--- src/main/scala/com/fellowship/UserRegistration.scala
+++ src/main/scala/com/fellowship/UserRegistration.scala
@@ -2,12 +2,15 @@ package com.fellowship
 class UserRegistration {
   val Name = "^[A-Z]{1}[a-z]{2,}$"
   val Email = "^[A-Za-z0-9+_.-]+@(.+)$"
+  // Validates Firstname
   def validateFirstName(firstName: String): Boolean ={
     firstName.matches(Name)
   }
+  // Validates Lastname
   def validateLastName(lastName: String): Boolean ={
     lastName.matches(Name)
   }
+  // Validates Email
   def validateEmail(email: String): Boolean ={
     email.matches(Email)
   }
